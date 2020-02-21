9cc00d3e8e2bac1cd9869710d4976bc27455d097
xrudoga <defcw58@gmail.com>
2020-02-22 01:12:04 +0800

    initial commit

---
+++ routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const { signup, signin } = require('../controllers/user');
+const { userSignupValidator } = require('../validator')
+
+// routes
+router.post("/signup", userSignupValidator,signup);
+router.post("/signin", signin);
+
+module.exports = router;
