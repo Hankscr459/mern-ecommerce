1c5f686a985a2a47707079d4fc8492567f3bd5c9
xrudoga <defcw58@gmail.com>
2020-02-24 17:14:06 +0800

    Node - Sending Products with Queries

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove, update } = require('../controllers/product');
-const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+const {
+    create,
+    productById,
+    read,
+    remove,
+    update,
+    list,
+    listRelated,
+    listCategories,
+    listBySearch,
+    photo
+} = require('../controllers/product');
+const {
+    requireSignin,
+    isAuth,
+    isAdmin
+} = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 router.get('/product/:productId', read)
@@ -12,7 +27,7 @@ router.post(
     isAuth,
     isAdmin,
     create
-);
+)
 router.delete(
     '/product/:productId/:userId',
     requireSignin,
@@ -20,7 +35,6 @@ router.delete(
     isAdmin,
     remove
 )
-
 router.put(
     '/product/:productId/:userId',
     requireSignin,
@@ -28,7 +42,11 @@ router.put(
     isAdmin,
     update
 )
-
+router.get('/products', list)
+router.get('/products/related/:productId', listRelated)
+router.get('/products/categories', listCategories)
+router.post('/products/by/search', listBySearch)
+router.get('/product/photo/:productId', photo)
 
 router.param('userId', userById);
 router.param('productId', productById);
