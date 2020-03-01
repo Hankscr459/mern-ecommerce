bfb5e31b07c1724878f69a911e27d3fe933e7d30
xrudoga <defcw58@gmail.com>
2020-03-01 23:31:14 +0800

    save order in the database

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
 const categoryRoutes = require('./routes/category')
 const productRoutes = require('./routes/product')
+const braintreeRoutes = require('./routes/braintree')
+const orderRoutes = require('./routes/order')
 // app
 const app = express();
 
@@ -29,11 +31,14 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
+
 // routes middlemare
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
+app.use('/api', braintreeRoutes);
+app.use('/api', orderRoutes);
 
 const port = process.env.PORT || 8000
 
--- controllers/category.js
+++ controllers/category.js
@@ -2,15 +2,16 @@ const Category = require('../models/category');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.categoryById = (req, res, next, id) => {
-    Category.findById(id).exec((err, category) => {
-        if (err || ! category) {
-            return res.status(400).json({
-                error: 'Category does not exist'
-            })
-        }
-        req.category = category;
-        next();
-    })
+    Category.findById(id)
+        .exec((err, category) => {
+            if (err || ! category) {
+                return res.status(400).json({
+                    error: 'Category does not exist'
+                })
+            }
+            req.category = category;
+            next();
+        })
 }
 
 exports.create = (req, res) => {
