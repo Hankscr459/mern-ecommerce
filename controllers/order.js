6f3cf1985c65779ae8efcc667c46796c42bfecea
xrudoga <defcw58@gmail.com>
2020-03-03 22:38:26 +0800

    Find order by id and update order status

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,19 @@
-const { Order, CartIten } = require('../models/order')
+const { Order, CartItem } = require('../models/order')
 const { errorHandler } = require('../helpers/dbErrorHandler')
+
+exports.orderById = (req, res, next, id) => {
+    Order.findById(id)
+        .populate('products.product', 'name price')
+        .exec((err, order) => {
+            if(err || !order) {
+                return res.status(400).json({
+                    error: errorHandler(error)
+                })
+            }
+            req.order = order
+            next()
+        })
+}
 
 exports.create = (req, res) => {
     // console.log('Create Order: ', req.body)
@@ -27,4 +41,23 @@ exports.listOrders = (req, res) => {
         }
         res.json(orders)
     })
+}
+
+exports.getStatusValues = (req, res) => {
+    res.json(Order.schema.path('status').enumValues)
+}
+
+exports.updateOrderStatus = (req, res) => {
+    Order.update(
+        { _id: req.body.orderId},
+        { $set: { status: req.body.status }},
+        (err,order) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(error)
+                })
+            }
+            res.json(order)
+        }
+    )
 }
